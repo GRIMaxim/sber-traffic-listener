fbb6b34113642f72701be4c106af16a03d358cd8
Grigoruk Maxim <grimax14@gmail.com>
2023-09-26 22:13:15 +0300

    Дополнены обработчики

--- src/traffic_listener/router.py
+++ src/traffic_listener/router.py
@@ -1,14 +1,21 @@
 from fastapi import APIRouter, status
 
+from ..default_responses import OkResponse
+from schemas import DomainCreateMany, DomainReadAll
+from crud import domain_db
 
 router = APIRouter()
 
 
-@router.post("/visited_links", status_code=status.HTTP_201_CREATED)
-async def add_links():
-    pass
+@router.post(
+    "/visited_links", status_code=status.HTTP_201_CREATED, response_model=OkResponse
+)
+async def add_links(links: DomainCreateMany):
+    await domain_db.create_many(links)
+    return OkResponse
 
 
-@router.get("/visited_links", status_code=status.HTTP_200_OK)
+@router.get("/visited_links", status_code=status.HTTP_200_OK, response_model=DomainReadAll)
 async def get_links_by_period(start: int, end: int):
-    pass
+    await domain_db.get_by_time_delta(start, end)
+    return {"message": "hello"}
