9a00c08b7b32fef18fe32ffdccfd80bd89be5238
Grigoruk Maxim <grimax14@gmail.com>
2023-09-26 22:14:08 +0300

    Добавлена директория для тестов traffic_listener

---
+++ tests/traffic_listener/test_router.py
@@ -0,0 +1,9 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+from httpx import Response
+
+
+class TestRouter:
+
+    def test_add_visited_links(self, client: TestClient):
+        pass
