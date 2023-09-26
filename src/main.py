ad8dc6bfef9398b2dee03cf903f4e00a95ad896b
Grigoruk Maxim <grimax14@gmail.com>
2023-09-26 22:04:57 +0300

    Добавлен объект главного приложения

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from traffic_listener.router import router as tl_router
+from .traffic_listener.router import router as tl_router
 
 
 def get_app() -> FastAPI:
@@ -8,3 +8,6 @@ def get_app() -> FastAPI:
     app = FastAPI()
     app.include_router(tl_router)
     return app
+
+
+main_app = get_app()
