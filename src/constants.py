4af903922cae8c922c6d71be1a354fd5800f7987
Grigoruk <grimax14@gmail.com>
2023-09-26 16:14:48 +0300

    Базовая конфигурация базы данных

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+from sqlalchemy.sql.schema import _NamingSchemaParameter as NamingSchemaParameter
+from sqlalchemy.util import immutabledict
+
+DB_NAMING_CONVENTION: NamingSchemaParameter = immutabledict({
+    "ix": "%(column_0_label)s_idx",
+    "uq": "%(table_name)s_%(column_0_name)s_key",
+    "ck": "%(table_name)s_%(constraint_name)s_check",
+    "fk": "%(table_name)s_%(column_0_name)s_fkey",
+    "pk": "%(table_name)s_%(column_0_name)s_pkey",
+})
---
+++ src/database_config.py
@@ -0,0 +1,28 @@
+import os
+
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import AsyncAttrs, AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from src.constants import DB_NAMING_CONVENTION
+
+db_url: str = (
+    f"{os.getenv('PG_DRIVERNAME')}://"
+    f"{os.getenv('PG_USER')}:{os.getenv('PG_PASS')}@"
+    f"{os.getenv('PG_HOST')}:{os.getenv('PG_PORT')}/{os.getenv('PG_DATABASE')}"
+)
+engine: AsyncEngine = create_async_engine(db_url, echo=True, future=True)
+async_session_maker: async_sessionmaker[AsyncSession] = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+my_metadata = MetaData(naming_convention=DB_NAMING_CONVENTION)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    """Базовый класс для всех создаваемых схем данных."""
+
+    metadata = my_metadata
+
+    pk: Mapped[int] = mapped_column(primary_key=True)
