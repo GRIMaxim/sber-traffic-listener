f82271e94dcf94219e275c217af0523e763bdd0e
Grigoruk <grimax14@gmail.com>
2023-09-28 15:36:06 +0300

    Добавлены ошибки и их обработчики

---
+++ src/traffic_listener/exceptions.py
@@ -0,0 +1,34 @@
+from fastapi import Request, status
+from fastapi.responses import JSONResponse
+
+from .constants import ErrorMessages
+
+
+class ListIsEmptyError(Exception):
+    """Исключение используется в случае, если список ссылок пуст."""
+
+
+class ListIsNotContainsLinksError(Exception):
+    """Исключение используется в случае, если список ссылок содержит только слова."""
+
+
+class IncorrectParametersError(Exception):
+    """Исключение используется в случае, если параметр *from* больше, чем *to*."""
+
+
+async def empty_list_exception_handler(request: Request, exc: ListIsEmptyError) -> JSONResponse:  # noqa: ARG001
+    """Обработчик для исключения ListIsEmptyError."""
+    return JSONResponse(status_code=status.HTTP_411_LENGTH_REQUIRED,
+                        content={"status": ErrorMessages.list_is_empty})
+
+
+async def word_list_exception_handler(request: Request, exc: ListIsNotContainsLinksError) -> JSONResponse:  # noqa: ARG001
+    """Обработчик для исключения ListIsNotContainsLinksError."""
+    return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
+                        content={"status": ErrorMessages.list_contains_only_words})
+
+
+async def incorrect_parameters_exception_handler(request: Request, exc: ListIsNotContainsLinksError) -> JSONResponse:  # noqa: ARG001
+    """Обработчик для исключения IncorrectParametersError."""
+    return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
+                        content={"status": ErrorMessages.incorrect_parameters})
