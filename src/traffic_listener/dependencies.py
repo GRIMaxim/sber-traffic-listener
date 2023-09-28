d4d569b493f11102b07a23c46cdfd15994b9823b
Grigoruk <grimax14@gmail.com>
2023-09-28 15:39:32 +0300

    Добавлены зависимости для валидации входных данных

---
+++ src/traffic_listener/dependencies.py
@@ -0,0 +1,26 @@
+from typing import Annotated, Sequence
+from urllib.parse import urlparse
+
+from fastapi import Query
+
+from .schemas import LinkCreateMany
+from .exceptions import ListIsEmptyError, ListIsNotContainsLinksError, IncorrectParametersError
+
+
+async def check_links(input_links: LinkCreateMany) -> list[str]:
+    """Валидация на корректность ввода url (есть ли домен) и на пустой список."""
+    links = [link for link in input_links.links if urlparse(link).hostname is not None]
+    if not input_links.links:
+        raise ListIsEmptyError
+    if not links:
+        raise ListIsNotContainsLinksError
+    return links
+
+
+async def check_parameters(start: Annotated[int | None, Query(alias="from", example=1234567890)] = None,
+                           end: Annotated[int | None, Query(alias="to", example=2345678901)] = None) \
+        -> Sequence[int | None]:
+    """Валидация на корректность ввода параметров."""
+    if start and end and start > end:
+        raise IncorrectParametersError
+    return start, end
