e2bca0eaa544da24678eddcb32695eac31e1b70d
Grigoruk <grimax14@gmail.com>
2023-09-28 15:38:59 +0300

    Добавлена загрузка переменных среды из .env

--- src/database_config.py
+++ src/database_config.py
@@ -3,15 +3,18 @@ import os
 from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import AsyncAttrs, AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from dotenv import load_dotenv
 
 from src.constants import DB_NAMING_CONVENTION
+
+load_dotenv()
 
 db_url: str = (
     f"{os.getenv('PG_DRIVERNAME')}://"
     f"{os.getenv('PG_USER')}:{os.getenv('PG_PASS')}@"
-    f"{os.getenv('PG_HOST')}:{os.getenv('PG_PORT')}/{os.getenv('PG_DATABASE')}"
+    f"{os.getenv('PG_HOST', default='localhost')}:{os.getenv('PG_PORT')}/{os.getenv('PG_DATABASE')}"
 )
-engine: AsyncEngine = create_async_engine(db_url, echo=bool(os.getenv("F_TEST")), future=True)
+engine: AsyncEngine = create_async_engine(db_url, echo=os.getenv("F_TEST") == "true", future=True)
 async_session_maker: async_sessionmaker[AsyncSession] = async_sessionmaker(
     engine,
     class_=AsyncSession,
