0e539b4f775c3d09e25bfaa45f1222549abf77bf
Grigoruk Maxim <grimax14@gmail.com>
2023-09-30 15:21:56 +0300

    Добавление валидации дял схем запрсоов/ответов

--- src/traffic_listener/schemas.py
+++ src/traffic_listener/schemas.py
@@ -1,31 +1,28 @@
-from pydantic import BaseModel, ConfigDict
-
-from src.traffic_listener.constants import ErrorMessages
+from pydantic import BaseModel, ConfigDict, AnyUrl, Field
+from .constants import ErrorMessages
 
 
 class LinkCreate(BaseModel):
     """Модель запроса для создания записи в таблице Link."""
 
-    link: str
+    link: AnyUrl
 
 
 class LinkCreateMany(BaseModel):
     """Модель запроса для создания нескольких записей в таблице Link."""
 
-    links: list[str]
-
-    model_config = {
-        "json_schema_extra": {
-            "examples": [{
-                "links": [
+    links: list[AnyUrl] = Field(
+        min_length=1,
+        json_schema_extra={
+            "examples": [
+                [
                     "http://yandex.ru/1",
-                    "https://ya.ru/maps",
-                    "http://sber.com/",
+                    "http://yandex.ru/2",
+                    "http://yandex.ru/3",
                 ],
-            },
             ],
         },
-    }
+    )
 
 
 class LinkRead(LinkCreate):
@@ -42,35 +39,27 @@ class LinkUpdate(LinkRead):
 class DomainReadAll(BaseModel):
     """Модель ответа при запросе доменов."""
 
-    domains: list[str]
-
-    model_config = {
-        "json_schema_extra": {
-            "examples": [{
-                "domains": [
+    domains: list[str] = Field(
+        json_schema_extra={
+            "examples": [
+                [
                     "yandex.ru",
                     "ya.ru",
                     "sber.com",
                 ],
-            },
             ],
         },
-    }
+    )
+    status: str
 
 
-class ListIsEmpty(BaseModel):
-    """Модель ответа для ошибки ListIsEmptyError (используется только для документации)."""
+class IncorrectLinks(BaseModel):
+    """Модель ответа при возникновении ошибки с ссылками."""
 
-    status: str = ErrorMessages.list_is_empty
-
-
-class ListContainOnlyWords(BaseModel):
-    """Модель ответа для ошибки ListIsNotContainsLinksError (используется только для документации)."""
-
-    status: str = ErrorMessages.list_contains_only_words
+    status: str = ErrorMessages.incorrect_links
 
 
 class IncorrectParameters(BaseModel):
-    """Модель ответа для ошибки IncorrectParametersError (используется только для документации)."""
+    """Модель ответа при возникновении ошибки."""
 
     status: str = ErrorMessages.incorrect_parameters
