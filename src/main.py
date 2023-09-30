d1d7f8f515430130752103ef192e8227350a1b18
Grigoruk Maxim <grimax14@gmail.com>
2023-09-30 15:21:18 +0300

    Изменение обработчиков исключений

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 
 from .traffic_listener.router import router as tl_router
-from .traffic_listener.exceptions import (ListIsEmptyError,
-                                          empty_list_exception_handler,
-                                          ListIsNotContainsLinksError,
-                                          word_list_exception_handler,
-                                          IncorrectParametersError,
-                                          incorrect_parameters_exception_handler)
+from .traffic_listener.exceptions import (
+    IncorrectParametersError,
+    incorrect_parameters_exception_handler,
+    incorrect_link_list_exception_handler,
+)
 
 
 def get_app() -> FastAPI:
@@ -14,9 +14,12 @@ def get_app() -> FastAPI:
     app = FastAPI(title="Traffic listener")
 
     app.include_router(tl_router)
-    app.add_exception_handler(ListIsEmptyError, empty_list_exception_handler)
-    app.add_exception_handler(ListIsNotContainsLinksError, word_list_exception_handler)
-    app.add_exception_handler(IncorrectParametersError, incorrect_parameters_exception_handler)
+    app.add_exception_handler(
+        RequestValidationError, incorrect_link_list_exception_handler,
+    )
+    app.add_exception_handler(
+        IncorrectParametersError, incorrect_parameters_exception_handler,
+    )
 
     return app
 
