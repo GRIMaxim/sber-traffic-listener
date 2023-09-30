9039d18c390324a2a071fb15eb49be9bb151198f
Grigoruk Maxim <grimax14@gmail.com>
2023-09-30 15:15:09 +0300

    Изменен CRUDLink

--- src/traffic_listener/crud.py
+++ src/traffic_listener/crud.py
@@ -1,35 +1,32 @@
 from collections.abc import Sequence
-from urllib.parse import urlparse
 from time import time
 
 from sqlalchemy import insert, select
+from pydantic import AnyUrl
 
 from src.crud import CRUDBase
 from src.utils import async_execute
 from .database import Link
-from .schemas import LinkCreate, LinkUpdate
+from .schemas import LinkCreate, LinkUpdate, LinkCreateMany
 
 
 class CRUDLink(CRUDBase[Link, LinkCreate, LinkUpdate]):
     """Расширение CRUDBase для таблицы Link."""
 
-    async def create_many(self, data_in: Sequence[str]) -> list[str]:
+    async def create_many(self, data_in: LinkCreateMany) -> None:
         """Запись нескольких ссылок в таблицу Link."""
-        values = [{"visit_time": int(time()), "link": link} for link in data_in]
+        values = [{"visit_time": int(time()), "link": link.unicode_string(), "domain": link.host} for link in data_in.links]
 
         query = insert(self.model).values(values).returning(self.model.link)
-        result = await async_execute(query)
-        created_links: list[str] = [link[0] for link in result.all()]
+        await async_execute(query)
 
-        return created_links
-
-    async def get_unique_domains_by_period(self, start: int | None = None, end: int | None = None) -> list[str]:
+    async def get_unique_domains_by_period(self, start: int | None = None, end: int | None = None) -> Sequence[str]:
         """Получение списка уникальных доменов из таблицы Link по заданному диапазону.
 
         **Параметры**
 
         *start* - минимальное значение диапазона для получения доменов.
-        Если start не задан, установится значение end - 10 (секунд)
+        Если start не задан, установится значение end-10 (секунд)
 
         *end* - максимально значение диапазона для получения доменов.
         Если end не задан, установится значение int(time()) на момент вызова
@@ -39,12 +36,11 @@ class CRUDLink(CRUDBase[Link, LinkCreate, LinkUpdate]):
         if not start:
             start = end - 10
 
-        query = select(self.model.link).where((self.model.visit_time >= start) & (self.model.visit_time <= end))
+        query = (select(self.model.domain).
+                 where((self.model.visit_time >= start) & (self.model.visit_time <= end)).
+                 distinct())
         result = await async_execute(query)
-        getting_links: list[str] = [link[0] for link in result.all()]
-        return list(
-            {domain for link in getting_links if (domain := urlparse(link).hostname) is not None})
-
+        return result.scalars().all()
 
 
 link_db = CRUDLink(Link)
