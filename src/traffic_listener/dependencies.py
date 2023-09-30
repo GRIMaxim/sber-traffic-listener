ce26c3a2c4800c2f338d3b4a711ca8240f59e2e8
Grigoruk Maxim <grimax14@gmail.com>
2023-09-30 15:20:27 +0300

    Перенесение валидации в pydantic

--- src/traffic_listener/dependencies.py
+++ src/traffic_listener/dependencies.py
@@ -1,21 +1,9 @@
 from typing import Annotated
 from collections.abc import Sequence
-from urllib.parse import urlparse
 
 from fastapi import Query
 
-from .schemas import LinkCreateMany
-from .exceptions import ListIsEmptyError, ListIsNotContainsLinksError, IncorrectParametersError
-
-
-async def check_links(input_links: LinkCreateMany) -> list[str]:
-    """Валидация на корректность ввода url (есть ли домен) и на пустой список."""
-    links = [link for link in input_links.links if urlparse(link).hostname is not None]
-    if not input_links.links:
-        raise ListIsEmptyError
-    if not links:
-        raise ListIsNotContainsLinksError
-    return links
+from .exceptions import IncorrectParametersError
 
 
 async def check_parameters(start: Annotated[int | None, Query(alias="from")] = None,
